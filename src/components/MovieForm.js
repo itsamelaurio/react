19c73af6ce3993f72e1ef327f71e320ff8d48c86
itsamelaurio <mslaurabarba@gmail.com>
2021-05-11 17:38:34 +0200

    lade till validation

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,15 +1,33 @@
 import React from 'react'
 
 export default function MovieForm() {
+
+    const [title, setTitle] = React.useState("");
+    const [rating, setRating] = React.useState(0);
+
+    function checkForm (e){
+        e.preventDefault();
+
+        if (title === "" ){
+            alert("Lägg till en titel");
+        }else if(parseInt(rating,10) === 0){
+            alert("Lägg till ett betyg");
+        }else{
+            setTitle("");
+            setRating(0);
+            document.getElementById("add-movie").reset();
+            alert("Success");
+        }
+    }
+
     return (
-        <div>
-            <form id="add-movie">
+            <form id="add-movie" onSubmit = {checkForm}>
                 <fieldset>
                     <legend>Lägg till en film</legend>
                     <label htmlFor="title">Titel:</label>
-                    <input type="text" id="title" className="form-control" />
+                    <input type="text" id="title" className="form-control" onChange={e => setTitle(e.target.value)}/>
                     <label htmlFor="rating">Betyg:</label>
-                    <select type="text" id="rating" className="form-control">
+                    <select type="text" id="rating" className="form-control" onChange={e => setRating(e.target.value)}>
                         <option value={0}>Välj betyg här...</option>
                         <option value={1}>1</option>
                         <option value={2}>2</option>
@@ -17,9 +35,8 @@ export default function MovieForm() {
                         <option value={4}>4</option>
                         <option value={5}>5</option>
                     </select>
-                    <input type="submit" class="btn btn-success mt-3" value="Spara film"></input>
+                    <input type="submit" className="btn btn-success mt-3" defaultValue="Spara film"></input>
                 </fieldset>
             </form>
-        </div>
     )
 }
