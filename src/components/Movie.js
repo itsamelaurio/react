0a5138c747e056ddd5f7bd030f35bec43e8f626b
itsamelaurio <mslaurabarba@gmail.com>
2021-05-11 18:54:17 +0200

    lade till stjärnor till filmerna

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 export default function Movie(props) {
+    let [stars, setStars] = React.useState("");
+
+    React.useEffect(()=>{
+        let star = []
+        for(let i = 0; i < props.movie.rating;i++){
+           star.push(<i className="fa fa-star checked" style={{color: "orange"}}></i>);
+        }
+        setStars(star);
+        
+    }, [props.movie] );
+    
+
     return (
         <li className="list-group-item">
             { props.movie.title }
-            
+            <span id = "stars">
+                {stars}
+            </span>
             <button className="btn btn-sm btn-danger float-end" onClick={() => props.deleteMovie(props.movie.id)}>Ta Bort</button>
         </li>
     )
